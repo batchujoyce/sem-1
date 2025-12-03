67fd073e3e48bb6b39fbc8d574c87221fbc94ab6
batchujoyce <batchujoyce512@gmail.com>
2025-12-03 20:49:09 +0530

    Add files via upload

---
+++ elgibility for voting.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	age=18;
+	printf("you are elgible to vote");
+	return 0;
+}
---
+++ minimum value.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{int a,b,p,i;
+      scanf("%d%d",&b,&a);
+      for(i=1;i<=b;i++)
+      {
+      	p=a+p;
+      
+	  }
+	  	printf("the product  %d",p);
+	  return 0;
+	  }

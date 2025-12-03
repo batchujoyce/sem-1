1fcfd5074a408c8fda3e2d2018a7bc92b32e99fb
batchujoyce <batchujoyce512@gmail.com>
2025-12-03 21:04:34 +0530

    Add files via upload

---
+++ auto keyword.cpp
@@ -0,0 +1,11 @@
+//write a C program for auto keyword
+#include<stdio.h>
+void display()
+{
+	auto int a=10;
+	printf("%d",a);
+}
+int main()
+{
+	display();
+}
---
+++ biggest_of_2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,big;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+    if(a>b){
+    	printf("a is greater");
+	}
+	else
+	{
+		printf("b is greater");
+	}
+    return 0;
+}
